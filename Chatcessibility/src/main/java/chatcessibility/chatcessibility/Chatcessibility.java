c152fc55bd55886b0b182274b0be2378d56c002b
PreceededMold155 <68513186+PreceededMold155@users.noreply.github.com>
2021-08-10 14:51:26 -0400

    Added files to the Repository
    People can now help with the project

---
+++ Chatcessibility/src/main/java/chatcessibility/chatcessibility/Chatcessibility.java
@@ -0,0 +1,16 @@
+package chatcessibility.chatcessibility;
+import org.bukkit.conversations.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chatcessibility extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ Chatcessibility/src/main/java/chatcessibility/chatcessibility/contextEvaluationFunction.java
@@ -0,0 +1,17 @@
+package chatcessibility.chatcessibility;
+import org.bukkit.util.io.BukkitObjectInputStream;
+
+import java.io.ObjectInputStream;
+import java.util.function.Function;
+
+public class contextEvaluationFunction {
+    /*
+    This project requires this file as it is needed to process
+    any and all server input via command line. When the server
+    sends a command over the internet. the plugin will listen to
+    chat to make sure that the user is able to essentially
+    receive the accessibility service correctly. this file is
+    responsible for everything
+    */
+     
+}
